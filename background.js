d8d5f9c0d06c63576c81e071ba2e72044450ea79
Johan Laursen <johanfsl@gmail.com>
2013-08-23 08:46:13 +0200

    update the context menu on focuschanged event

--- background.js
+++ background.js
@@ -1,44 +1,50 @@
 var openActive = false;
 
-function tabOpenerFunction (id) {
-  return function (event) {
-    var url = event.linkUrl;
+function tabOpenerFunction (windowId) {
+  return function (onClickEvent) {
+    var url = onClickEvent.linkUrl;
 
     chrome.tabs.create({
-      windowId: id,
+      windowId: windowId,
       url: url,
       active: openActive
     });
   };
 }
 
-var mainMenu = chrome.contextMenus.create({
-  title: 'Open in specific tab',
-  contexts: ['link'],
-  onclick: function (event) {
-    console.log('hello', mainMenu);
-  }
-});
-
-chrome.windows.getAll(function (windows) {
-  var i       = 0,
-      title   = '',
-      height  = 0,
-      width   = 0,
-      id      = 0;
-  
-  for (i = windows.length-1; i >= 0 ; i--) {
-    id      = windows[i].id;
-    height  = windows[i].height;
-    width   = windows[i].width;
-
-    title = '' + id + ' (' + height + ' x ' + width + ') ';
-
-    chrome.contextMenus.create({
-      title: title,
-      contexts: ['link'],
-      onclick: tabOpenerFunction(id),
-      parentId: mainMenu
+function updateMenu (focusChangedEvent) {
+  chrome.contextMenus.removeAll(function () {
+    var mainMenu = chrome.contextMenus.create({
+      title: 'Open in specific tab',
+      contexts: ['link']
     });
-  }
-});
+
+    chrome.windows.getAll(function (windows) {
+      var i       = 0,
+          title   = '',
+          height  = 0,
+          width   = 0,
+          id      = 0;
+      
+      for (i = windows.length-1; i >= 0 ; i--) {
+        id      = windows[i].id;
+        height  = windows[i].height;
+        width   = windows[i].width;
+
+        title = '' + id + ' (' + width + ' x ' + height + ') ';
+
+        chrome.contextMenus.create({
+          title: title,
+          contexts: ['link'],
+          onclick: tabOpenerFunction(id),
+          parentId: mainMenu
+        });
+      }
+    });
+  });
+}
+
+
+updateMenu();
+
+chrome.windows.onFocusChanged.addListener(updateMenu);

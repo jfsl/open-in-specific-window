d2699d2f0939e3a1e348ee5ea3cb1ded126047d4
jfsl <johan@energycodes.dk>
2018-02-20 12:37:08 +0100

    added type information

--- background.js
+++ background.js
@@ -1,21 +1,19 @@
+// @ts-check
 var openActive = false;
 var windowTitles = [];
 
-chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "open-in-specific-window");
-  
-  port.onMessage.addListener(function(msg) {
-    if (msg.action == 'rename') {
-      windowTitles[msg.id] = msg.name;
-      updateMenu();
-    } else if (msg.action == 'get-name') {
-      port.postMessage(windowTitles[msg.id]);  
-    }
-  });
-});
-
-function tabOpenerFunction (windowId) {
-  return function (onClickEvent) {
+/**
+ * Creates a new tab in the selected window, using the attached linkUrl from the Event 
+ * 
+ * @param {Number} windowId 
+ * @return {Function}
+ */
+function tabOpenerFunction(windowId) {
+  /**
+   * 
+   * @param {chrome.contextMenus.OnClickData} onClickEvent 
+   */
+  var openerHandler = function (onClickEvent) {
     var url = onClickEvent.linkUrl;
 
     chrome.tabs.create({
@@ -24,9 +22,15 @@ function tabOpenerFunction (windowId) {
       active: openActive
     });
   };
+
+  return openerHandler;
 }
 
-function updateMenu (focusChangedEvent) {
+/**
+ * Updates the context menu in the chrome right-click GUI
+ * @param {chrome.windows.WindowIdEvent} focusChangedEvent 
+ */
+function updateMenu(focusChangedEvent) {
   chrome.contextMenus.removeAll(function () {
     var mainMenu = chrome.contextMenus.create({
       title: 'Open in specific window',
@@ -34,23 +38,23 @@ function updateMenu (focusChangedEvent) {
     });
 
     chrome.windows.getAll(function (windows) {
-      var i       = 0,
-          title   = '',
-          height  = 0,
-          width   = 0,
-          id      = 0;
+      var i = 0,
+        title = '',
+        height = 0,
+        width = 0,
+        id = 0;
 
-      for (i = windows.length-1; i >= 0 ; i--) {
+      for (i = windows.length - 1; i >= 0; i--) {
         chrome.tabs.query({
-            active: true,
-            windowId: windows[i].id
-          },
+          active: true,
+          windowId: windows[i].id
+        },
 
           function (result) {
             id = result[0].windowId;
             height = result[0].height;
             width = result[0].width;
-            tabTitle = result[0].title;
+            var tabTitle = result[0].title;
 
             if (windowTitles[result[0].windowId]) {
               tabTitle = windowTitles[result[0].windowId];
@@ -70,6 +74,19 @@ function updateMenu (focusChangedEvent) {
     });
   });
 }
+
+chrome.runtime.onConnect.addListener(function (port) {
+  console.assert(port.name == "open-in-specific-window");
+
+  port.onMessage.addListener(function (msg) {
+    if (msg.action == 'rename') {
+      windowTitles[msg.id] = msg.name;
+      updateMenu();
+    } else if (msg.action == 'get-name') {
+      port.postMessage(windowTitles[msg.id]);
+    }
+  });
+});
 
 updateMenu();
 chrome.windows.onFocusChanged.addListener(updateMenu);

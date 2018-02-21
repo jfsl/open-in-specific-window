6772bc519163443a0c69d31f33f68aff24b723db
jfsl <johan@energycodes.dk>
2018-02-21 13:19:34 +0100

    added external api through chrome messaging

--- background.js
+++ background.js
@@ -1,6 +1,19 @@
-// @ts-check
-var openActive = false;
-var windowTitles = [];
+var openActive = false; // Defines wether or not 
+var windowTitles = []; // Array of windowId -> name associations if the windows have been named by the user
+
+/**
+ * Creates a new chrome tab in the specified window id
+ *  
+ * @param {Number} windowId 
+ * @param {String} url 
+ */
+function createTab(windowId, url) {
+  chrome.tabs.create({
+    windowId: windowId,
+    url: url,
+    active: openActive
+  });
+}
 
 /**
  * Creates a new tab in the selected window, using the attached linkUrl from the Event 
@@ -15,12 +28,7 @@ function tabOpenerFunction(windowId) {
    */
   var openerHandler = function (onClickEvent) {
     var url = onClickEvent.linkUrl;
-
-    chrome.tabs.create({
-      windowId: windowId,
-      url: url,
-      active: openActive
-    });
+    createTab(windowId, url);
   };
 
   return openerHandler;
@@ -45,11 +53,11 @@ function updateMenu(focusChangedEvent) {
         id = 0;
 
       for (i = windows.length - 1; i >= 0; i--) {
-        chrome.tabs.query({
-          active: true,
-          windowId: windows[i].id
-        },
-
+        chrome.tabs.query(
+          {
+            active: true,
+            windowId: windows[i].id
+          },
           function (result) {
             id = result[0].windowId;
             height = result[0].height;
@@ -75,18 +83,76 @@ function updateMenu(focusChangedEvent) {
   });
 }
 
+// Listen for messages sent from the browserAction
 chrome.runtime.onConnect.addListener(function (port) {
   console.assert(port.name == "open-in-specific-window");
 
   port.onMessage.addListener(function (msg) {
     if (msg.action == 'rename') {
+      // Rename action will associate a name with a given window id
       windowTitles[msg.id] = msg.name;
       updateMenu();
     } else if (msg.action == 'get-name') {
+      // Get name action will return the name associated with a given window id
       port.postMessage(windowTitles[msg.id]);
     }
   });
 });
 
+
+/**
+ * Finds the window id with the given title, returns false if not found 
+ * @param {String} title
+ * @returns {int|False} 
+ */
+function findWindowIdWithTitle(searchTitle) {
+  for (idx = 0; idx < windowTitles.length; idx++) {
+    if (windowTitles[idx] == searchTitle) {
+        return idx;
+    }
+  }
+
+  return false;
+}
+
+// Listen for external messages 
+chrome.runtime.onMessageExternal.addListener(
+  function (request, sender, sendResponse) {
+    // All responses echo back the request.id attribute
+    // This is to make integration easier when doing multiple requests
+    // on the client side.
+    var response = {
+      'requestId': request.id
+    };
+
+    if (request.action == 'getWindowTitles') {
+      // getWindowTitles action will return the array of titled windows, if
+      // there are none, an empty array will be returned
+      response.windowTitles = windowTitles;
+      sendResponse(response);
+    } else if (request.action == 'getWindowIdWithTitle') {
+      // getWindowIdWithTitle will return the windowId associated with the given
+      // title, if no match is found response will have the success attribute set to false
+      var windowId = findWindowIdWithTitle(request.title);
+
+      if (windowId) {
+        response.windowId = idx;
+        response.success = true;
+        sendResponse(response);
+      } else {
+        response.success = false;
+        response.message = "Window title: " + request.title + " not found";
+        sendResponse(response);
+      }
+    } else if (request.action == 'openTabInWindow') {
+      // openTavInWindow action will open the requested url in the requested window id
+      createTab(request.windowId, request.url);
+    }
+  }
+);
+
+// Manually update the menu on first load 
 updateMenu();
+
+// Listen for changes in window focus and update the menu every time it happens.
 chrome.windows.onFocusChanged.addListener(updateMenu);

57f5aeee47eecff0de3b3385e6c1964711eb96c8
jfsl <johan@energycodes.dk>
2018-07-18 22:12:51 +0200

    added ts-check to main.js and fixed errors

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,26 +1,44 @@
+//@ts-check 
 var port = chrome.runtime.connect({ name: "open-in-specific-window" });
-port.postMessage({action: 'shortcut-open-enabled'});
+port.postMessage({ action: 'shortcut-open-enabled' });
 
+/**
+ * 
+ * @param {MouseEvent} evt 
+ */
 function linkClickedHandler(evt) {
+    let link = /** @type {HTMLLinkElement} */ (evt.target);
     if (evt.altKey) {
         evt.preventDefault();
         port.postMessage({
             action: 'shortcut-open',
-            url: evt.target.href
+            url: link.href
         });
     }
 }
 
-port.onMessage.addListener(function (msg) {
+/** 
+ * @typedef {Object} BackgroundMessage 
+ * @property {String} action
+ * @property {*} response
+ */
+
+/**
+ * Handle incoming messages from the background page
+ * @param {BackgroundMessage} msg 
+ */
+function portMessageHandler (msg) {
     switch (msg.action) {
         case 'shortcut-open-enabled':
             if (msg.response) {
                 let links = document.getElementsByTagName('a');
                 for (let i = 0; i < links.length; i++) {
-                    link = links[i];
+                    let link = links[i];
                     link.addEventListener('click', linkClickedHandler);
                 }
             }
-        break;
+            break;
     }
-});
+}
+
+port.onMessage.addListener(portMessageHandler);
